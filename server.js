c4f9ace9195a404b420b4493643d888f1557f725
seunzone <darealseun@gmail.com>
2017-09-02 18:01:21 +0100

    some errors, buh I'll fix it

---
+++ server.js
@@ -0,0 +1,52 @@
+const express = require('express'),
+    app = express(),
+    server = require('http').createServer(app),
+    path    = require("path"),
+    io = require('socket.io').listen(server),
+    usernames = [];
+    
+
+
+
+
+app.get('/', (req, res) =>{
+    res.sendFile(path.join(__dirname+'/index.html'));
+});
+
+io.sockets.on('connection', (socket) =>{
+    console.log('Socket Connected');
+
+    socket.on('new user', (data, callback)=>{
+        if(usernames.indexOf(data) != -1){
+            callback(false);
+        } else {
+            callback(true);
+            socket.username = data;
+            usernames.push(socket.username);
+            updateUsernames();
+        }
+    });
+
+    //Update Usernames
+    function updateUsernames(){
+        io.sockets.emit('usernames', usernames);
+    }
+
+    //Send Message
+    socket.on('send message',(data) =>{
+        io.sockets.emit('new message', {msg: data});
+    });
+
+    //Disconnect Event
+    socket.on('disconnect', (data)=>{
+        if(!socket.username){
+            return; 
+        }
+        usernames.splice(usernames.indexOf(socket.username), 1);
+        updateUsernames();
+    });
+});
+
+server.listen(process.env.PORT || 3000, function () {
+console.log('Our Site is on Port 3000');
+});
